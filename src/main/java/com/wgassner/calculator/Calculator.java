ecfdedde336089e19921ceeb07e72314fbcdbbcc
wgassner <wolfgang.gassner@mic-cust.com>
2020-02-08 00:26:18 +0100

    Add sum logic, controller and unit test

---
+++ src/main/java/com/wgassner/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.wgassner.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
